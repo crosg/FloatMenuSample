17c1570eb055a3f6443c85dcd0ba2bbc2b52a261
yiming <18720625976@163.com>
2016-05-04 13:55:07 +0800

    remove yuewen copyright

--- FloatMenu/src/main/java/com/yw/game/floatmenu/OnMenuActionListener.java
+++ FloatMenu/src/main/java/com/yw/game/floatmenu/OnMenuActionListener.java
@@ -1,28 +1,3 @@
-/*
- *
- *  Copyright (c) 2015,2016, yuewen and/or its affiliates. All rights reserved.
- *  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
- *  This code is free software; you can redistribute it and/or modify it
- *  under the terms of the GNU General Public License version 3 only, as
- *  published by the Free Software Foundation.  yuewen designates this
- *  particular file as subject to the "Classpath" exception as provided
- *  by yuewen in the LICENSE file that accompanied this code.
- *
- *  This code is distributed in the hope that it will be useful, but WITHOUT
- *  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- *  version 3 for more details (a copy is included in the LICENSE file that
- *  * accompanied this code).
- *
- *  You should have received a copy of the GNU General Public License version
- *  * 3 along with this work; if not, write to the Free Software Foundation,
- *  Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *  Please contact yuewen, then mailto opensource@yuewen.com
- *  if you need additional information or have any questions.
- * /
- */
-
 package com.yw.game.floatmenu;
 
 
